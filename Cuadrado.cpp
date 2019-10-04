2740a811bea827510edcbdfcf4771f4fba5eb5a5
abdieloflores <abdiel_flores@outlook.com>
2019-10-04 17:25:26 -0500

    Creando Clase Cuadrado con sus respectivos atributos y metodos

--- Cuadrado.cpp
+++ Cuadrado.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
+class Cuadrado{
+    private:
+        float lado;
+    public:
+        Cuadrado(float);
+        void perimetro();
+        void area();
+};
+
 int main(){
 
     cin.ignore();
-    cout<<"Presiones cualquier tecla para continuar..."
+    cout<<"Presiones cualquier tecla para continuar...";
     cin.get();
     return 0;
 }

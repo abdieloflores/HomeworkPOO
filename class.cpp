97c0981e3a49e1b00421ab1882438b85859940af
abdieloflores <abdiel_flores@outlook.com>
2019-10-04 08:13:38 -0500

    Resubiendo Archivo CPP para Clase Rectangulos con sus Atributos y Metodos

---
+++ class.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangulo()
+{
+    //Atributos
+    private:
+        float largo, ancho;
+    //Metodos
+    public:
+        void perimetro();
+        void area();
+}
+
+Rectangulo::Rectangulo(float)
+
+int main()
+{
+
+
+    
+    return 0;
+}

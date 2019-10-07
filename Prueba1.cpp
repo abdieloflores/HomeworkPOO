932c41d85e52923244d675cdf26bdf88dd3d0232
abdieloflores <abdiel_flores@outlook.com>
2019-10-07 09:02:08 -0500

    Cree la estructura basica de cpp

---
+++ Prueba1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+}
+
+    cin.ignore();
+    cout<<endl<<"Presiones cualquier tecla para continuar...";
+    cin.get();
+    return 0;
+}

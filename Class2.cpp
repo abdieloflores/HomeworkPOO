2a8030b27ecf8f9643d8106f390a0129cb5f413a
abdieloflores <abdiel_flores@outlook.com>
2019-10-11 07:32:18 -0500

    Se creo la clase Person

---
+++ Class2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+//Todos los datos privados se llaman encapsulamiento.
+
+class Person{
+    //Atributos
+        int age;
+        string name;
+    //Metodos
+        Person(int, string);
+        void read();
+        void eat();
+        void run();
+
+};
+
+int main (){
+
+
+
+return 0;
+}

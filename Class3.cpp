446fee1a0cd654b1d92385ee4ce6d40701237925
abdieloflores <abdiel_flores@outlook.com>
2019-10-11 09:26:07 -0500

    se creo el constructor Date con una sola variable

--- Class3.cpp
+++ Class3.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 class Date{
-    
+
     private:
         int day, month, year;
     public:
         Date(int,int,int);
+        Date(long);
         void show();
 };
 
@@ -17,6 +18,16 @@ Date::Date(int _day,int _month, int _year){
     month = _month;
     year = _year;
 }
+Date::Date(long date){ 
+    day = int(date/1000000);
+    month = int((date-day*1000000)/10000);
+    year = int(date-(day*1000000)-(month*10000));
+}
+/*Date::Date(long date){ 
+    day = int (date-(year*10000)-(month*100));
+    month = int((date-year*10000)/100);
+    year = int(date/10000);
+}*/
 
 void Date::show(){
     cout<<"La fecha es: "<<day<<" / "<<month<<" / "<<year<<endl;
@@ -25,7 +36,11 @@ void Date::show(){
 int main (){
 
     Date today(11, 10, 2019);
+    Date today2(11102019);
+    Date today3(20191011);
     today.show();
+    today2.show();
+    today3.show();
 
 cin.ignore();
 cout<<"\n\nPresiones cualquier tecla para continuar..."<<endl;

41eb98686f349a969fdf9f7f2d19a2ca4bb3958a
abdieloflores <abdiel_flores@outlook.com>
2019-11-01 08:02:22 -0600

    Subiendo programa Dog con destructor

---
+++ Class5.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Dog{
+    private:
+        string name, breed;
+    public:
+        Dog(string,string);
+        ~Dog();
+        void showData();
+        void play();
+};
+
+Dog::Dog(string _name, string _breed){
+    name = _name;
+    breed = _breed;
+}
+Dog::~Dog(){
+}
+
+void Dog::showData(){
+    cout<<"---- SHOW DATA ----"<<endl;
+    cout<<"Name: "<<name<<endl;
+    cout<<"Breed: "<<breed<<endl;
+}
+
+void Dog::play(){
+    cout<<"\nEl perro "<<name<<" esta jugando."<<endl;
+}
+
+int main (){
+
+    Dog dog1("Alfredito","Chihuahua");
+
+    dog1.showData();
+    dog1.play();
+    dog1.~Dog();
+
+
+    cin.ignore();
+    cout<<"Prsione cualquier tecla para contnuar...";
+    cin.get();
+    return 0;
+}
